f7506da1be49f7406683891983445f3561256786
unknown <emprenderusinque@hotmail.com>
2023-01-04 13:49:32 -0500

    retorno de las funciones

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -1,5 +1,9 @@
 function saludar( nombre ) {
     console.log('Hola ' + nombre); 
+    return 1;
+
+    //esto nunca se va a ejecutar
+    // console.log('Hola')
 } 
 
 const saludar2 = function() {
@@ -14,7 +18,28 @@ const saludarFlecha2 = nombre  => {
     console.log('Hola', nombre)
 }
 
-saludar('eric')
+const retornoDeSaludar = saludar('eric')
+console.log(retornoDeSaludar)
 
-saludarFlecha();
-saludarFlecha2( 'melissa' );
+
+// saludarFlecha();
+// saludarFlecha2( 'melissa' );
+
+function sumar( a, b ) {
+    return a + b 
+}
+
+/* const sumar2 = ( a, b ) => {
+    return a + b 
+} */
+
+const sumar2 = ( a, b ) => a + b 
+
+function getAleatorio() {
+    return Math.random();
+}
+
+const getAleatorio2 = () => Math.random();
+
+console.log( getAleatorio() )
+console.log( getAleatorio2() )
