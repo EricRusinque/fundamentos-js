1f9ec5ee7078b20b8949b0090e313574dce6ebcb
unknown <emprenderusinque@hotmail.com>
2023-01-24 14:45:26 -0500

    metodos en las clases

--- assets/js/clases/class.js
+++ assets/js/clases/class.js
@@ -11,10 +11,22 @@ class Persona {
         this.codigo = codigo;
         this.frase =  frase;
     }
+
+    quienSoy() {
+        console.log(`Soy ${this.nombre} y mi identidad es ${ this.codigo }`)
+    }
+
+    miFrase(){
+        this.quienSoy()
+        console.log(`${ this.codigo } dice: ${ this.frase }`)
+    }
 }
 
-const spiderman = new Persona( 'Peter Parker', 'Spider', 'Soy tu amigable vecino Spiderman');
+const spiderman = new Persona( 'Peter Parker', 'Spiderman', 'Soy tu amigable vecino Spiderman');
 const ironman = new Persona( 'Tony Stark', 'Ironman', 'Yo soy ironman');
 
 console.log(spiderman)
 console.log(ironman)
+
+spiderman.quienSoy()
+ironman.miFrase()
